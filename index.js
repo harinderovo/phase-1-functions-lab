01275f6d8f733f89dd3e4ee00dd956d2dc666f73
Harinder Singh <harmiglany@yahoo.com>
2022-11-03 12:35:17 -0700

    commit tree f036fb14d039dade5f9b57038a25341b2a281039

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    let distance = someValue - 42
+    if (distance < 0) {
+        distance *= -1
+    }
+    return distance 
+}
+
+function distanceFromHqInFeet(street) {
+    let feet = distanceFromHqInBlocks(street) * 264
+    return feet 
+}
+
+function distanceTravelledInFeet(start, finish) {
+    return Math.abs (start - finish) * 264
+}
+
+function calculatesFarePrice(start, destination) {
+    let result = distanceTravelledInFeet(start, destination) 
+        if (result <= 400) {
+            return 0 
+    } 
+    else if (result > 400 && result <= 2000) {
+        return (result-400) *0.02
+    }
+    else if (result > 2000 && result <= 2500) {
+        return 25
+    }
+    else if (result > 2500) {
+        return ('cannot travel that far')
+    }
+}
